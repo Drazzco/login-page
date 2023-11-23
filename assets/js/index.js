7ff1422cdabbe8ef37867db40354deb5ef21cc8d
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2023-11-23 11:10:32 -0300

    create index.js and signin.html

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+if (localStorage.getItem("token") == null) {
+    alert("You need be logged in to access this page");
+    window.location = "./assets/html/signin.html";
+}
+
+let userLoggedIn = JSON.parse(localStorage.getItem("userLoggedIn"));
+
+let loggedIn = document.querySelector("#loggedIn");
+loggedIn.innerHTML = "Hi ${userLoggedIn.name}";
+
+function exit() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userLoggedIn");
+    window.location.href = "./assets/html/signin.html";
+}
